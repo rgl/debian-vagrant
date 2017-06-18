285e33febeff1fad21a414ba48cb74287f390083
Rui Lopes <rgl@ruilopes.com>
2017-06-18 11:02:54 +0100

    update to final 9 release

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this will update the debian.json file with the current netboot image checksum.
 # see https://www.debian.org/CD/verify
-# see http://cdimage.debian.org/debian-cd/8.6.0/amd64/iso-cd/
+# see http://cdimage.debian.org/debian-cd/9.0.0/amd64/iso-cd/
 set -eux
 iso_url=$(jq -r '.builders[0].iso_url' debian.json)
 iso_checksum_url="$(dirname $iso_url)/SHA256SUMS"
