51b2d8cf2eb2f0cacb64313bcbf6a6e83694bb97
Rui Lopes <rgl@ruilopes.com>
2017-12-16 08:46:10 +0000

    update to upstream release 9.3.0

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this will update the debian.json file with the current netboot image checksum.
 # see https://www.debian.org/CD/verify
-# see http://cdimage.debian.org/debian-cd/9.2.1/amd64/iso-cd/
+# see http://cdimage.debian.org/debian-cd/9.3.0/amd64/iso-cd/
 set -eux
 iso_url=$(jq -r '.variables.iso_url' debian.json)
 iso_checksum_url="$(dirname $iso_url)/SHA256SUMS"
