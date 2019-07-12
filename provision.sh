ed9a8e132b1e16b13e03d620ff9d417ffca336c7
Rui Lopes <rgl@ruilopes.com>
2019-07-12 19:30:24 +0100

    workaround the installation of the VirtualBox Guest Additions because I was not able to get Debian Buster d-i to ignore the second iso

--- provision.sh
+++ provision.sh
@@ -33,13 +33,18 @@ if [ -n "$(lspci | grep VirtualBox | head -1)" ]; then
 # REMOVE_INSTALLATION_DIR=0 is to fix a bug in VBoxLinuxAdditions.run.
 # See http://stackoverflow.com/a/25943638.
 apt-get -y -q install gcc dkms bzip2
-mkdir -p /mnt
-mount /dev/sr1 /mnt
-while [ ! -f /mnt/VBoxLinuxAdditions.run ]; do sleep 1; done
-REMOVE_INSTALLATION_DIR=0 /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions
+mkdir -p /media/iso
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /media/iso
+while [ ! -f /media/iso/VBoxLinuxAdditions.run ]; do sleep 1; done
+# NB we assume this command will always succeed due to:
+#       VirtualBox Guest Additions: Running kernel modules will not be replaced until the system is restarted
+REMOVE_INSTALLATION_DIR=0 \
+    /media/iso/VBoxLinuxAdditions.run \
+        --target /tmp/VBoxGuestAdditions \
+    || true
 rm -rf /tmp/VBoxGuestAdditions
-umount /mnt
-eject /dev/sr1
+umount /media/iso
+rm -rf /media/iso /home/vagrant/VBoxGuestAdditions.iso
 modinfo vboxguest
 elif [ -n "$(lspci | grep 'Red Hat' | head -1)" ]; then
 # install the qemu-kvm Guest Additions.
