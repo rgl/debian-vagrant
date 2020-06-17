0a82e868bb977f307ebdfa8f2a355544e92b6f67
Rui Lopes <rgl@ruilopes.com>
2020-06-17 08:46:42 +0100

    update to upstream release 10.4.0 and packer 1.6.0

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this will update the debian.json file with the current netboot image checksum.
 # see https://www.debian.org/CD/verify
-# see https://cdimage.debian.org/debian-cd/10.3.0/amd64/iso-cd/
+# see https://cdimage.debian.org/debian-cd/10.4.0/amd64/iso-cd/
 set -eux
 iso_url=$(jq -r '.variables.iso_url' debian.json)
 iso_checksum_url="$(dirname $iso_url)/SHA256SUMS"
@@ -11,7 +11,7 @@ gpg --keyserver hkp://keyring.debian.org/ --recv-keys 0x64E6EA7D 0x6294BE9B 0x09
 gpg --verify SHA256SUMS.sign SHA256SUMS
 iso_checksum=$(grep $(basename $iso_url) SHA256SUMS | awk '{print $1}')
 for f in debian*.json; do
-    sed -i -E "s,(\"iso_checksum\": \")([a-f0-9]+)(\"),\\1$iso_checksum\\3,g" $f
+    sed -i -E "s,(\"iso_checksum\": \")([a-z0-9:]+)(\"),\\1sha256:$iso_checksum\\3,g" $f
 done
 rm SHA256SUMS*
 echo 'iso_checksum updated successfully'
