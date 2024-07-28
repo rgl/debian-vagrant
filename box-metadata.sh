1462dd797090ddb3bcd172deb60680e55f9bb6e0
Rui Lopes <rgl@ruilopes.com>
2024-07-28 12:26:50 +0100

    declare the vagrant 1.1.4 plugin requirement

--- box-metadata.sh
+++ box-metadata.sh
@@ -7,7 +7,7 @@ path="$3"
 
 # see https://developer.hashicorp.com/vagrant/docs/boxes/format#box-metadata
 # see https://developer.hashicorp.com/vagrant/docs/boxes/format#box-file
-# see https://github.com/hashicorp/packer-plugin-vagrant/blob/v1.0.3/post-processor/vagrant/libvirt.go#L100-L105
+# see https://github.com/hashicorp/packer-plugin-vagrant/blob/v1.1.4/post-processor/vagrant/libvirt.go#L105-L109
 # see https://github.com/vagrant-libvirt/vagrant-libvirt/blob/0.11.2/spec/unit/action/handle_box_image_spec.rb#L96-L125
 # see https://github.com/vagrant-libvirt/vagrant-libvirt/blob/0.11.2/lib/vagrant-libvirt/action/handle_box_image.rb
 # see https://github.com/vagrant-libvirt/vagrant-libvirt/blob/0.11.2/docs/boxes.markdown
