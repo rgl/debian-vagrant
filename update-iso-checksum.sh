c65de1741223b62e40fd27e8da52573f9a53d8bf
Rui Lopes <rgl@ruilopes.com>
2025-06-01 07:08:08 +0000

    double quote the bash strings to prevent word splitting

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -4,14 +4,14 @@
 # see https://cdimage.debian.org/debian-cd/12.11.0/amd64/iso-cd/
 set -eux
 iso_url="$(perl -ne '/default\s*=\s*\"(https:.+\.debian\.org.+)\"/ && print $1' <debian.pkr.hcl)"
-iso_checksum_url="$(dirname $iso_url)/SHA256SUMS"
-curl -O --silent --show-error $iso_checksum_url
-curl -O --silent --show-error $iso_checksum_url.sign
+iso_checksum_url="$(dirname "$iso_url")/SHA256SUMS"
+curl -O --silent --show-error "$iso_checksum_url"
+curl -O --silent --show-error "$iso_checksum_url.sign"
 gpg --keyserver hkp://keyring.debian.org/ --recv-keys 0x64E6EA7D 0x6294BE9B 0x09EA8AC3
 gpg --verify SHA256SUMS.sign SHA256SUMS
-iso_checksum=$(grep $(basename $iso_url) SHA256SUMS | awk '{print $1}')
+iso_checksum="$(grep "$(basename "$iso_url")" SHA256SUMS | awk '{print $1}')"
 for f in debian*.pkr.hcl; do
-    sed -i -E "s,(default\s*=\s*\")(sha256:[a-z0-9]+)(\"),\\1sha256:$iso_checksum\\3,g" $f
+    sed -i -E "s,(default\s*=\s*\")(sha256:[a-z0-9]+)(\"),\\1sha256:$iso_checksum\\3,g" "$f"
 done
 rm SHA256SUMS*
 echo 'iso_checksum updated successfully'
